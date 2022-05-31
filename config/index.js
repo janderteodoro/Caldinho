6b07266608ef21d08e8dbd0afd145a74eda05247
janderteodoro <jandertheodoro@gmail.com>
2022-05-30 22:35:09 -0300

    nao sei

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const config = {
     uri: process.env.URI,
     port: process.env.PORT,
+    dbo: process.env.dbo,
 }
 
 module.exports = config;
--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const mongoCLient = require('mongodb').MongoClient;
 const app = express();
 const port = config.port;
 const uri = config.uri;
+const dataBase = config.dbo;
 const routes = require('./src/routes');
 
 app.use(bodyParser.json());
 
 routes.homePage(app);
-routes.register(app, mongoCLient, uri);
+routes.register({app, mongoCLient, uri, dataBase});
 
 app.listen(port, () => {
     console.log(`App runnig in http:/localhost:${port}`);
